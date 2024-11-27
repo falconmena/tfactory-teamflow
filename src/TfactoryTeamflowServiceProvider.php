be52ff8675107d1516e0794eadc79b2c90a935bb
a.almoghrabi <a.almoghrabi@techsfactory.com>
2024-11-27 23:58:25 +0300

    fixes

--- src/TfactoryTeamflowServiceProvider.php
+++ src/TfactoryTeamflowServiceProvider.php
@@ -12,59 +12,47 @@ class TfactoryTeamflowServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('tfactory-teamflow')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_tfactory_teamflow_table')
             ->hasCommand(TfactoryTeamflowCommand::class)
-            ->hasInstallCommand(function(InstallCommand $command) {
+            ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
                     ->publishAssets()
-                    ->publishMigrations();
+                    ->publishMigrations()
+                    ->runMigrations();
             });
     }
 
     public function boot(): void
     {
-        // Register Livewire Components
+        parent::boot();
+
         $this->registerLivewireComponents();
 
         $this->publishes([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'tfactory-teamflow-migrations');
 
-        // Register config file for publishing
         $this->publishes([
             __DIR__ . '/../config/tfactory-teamflow.php' => config_path('tfactory-teamflow.php'),
         ], 'tfactory-teamflow-config');
 
-        // Register views for publishing
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/tfactory-teamflow'),
         ], 'tfactory-teamflow-views');
     }
 
-    /**
-     * Register Livewire components for the package.
-     */
     protected function registerLivewireComponents()
     {
-        // Publish Livewire's assets if needed (optional)
         if (class_exists(\Livewire\Livewire::class)) {
             Livewire::component('send-message', \Techsfactory\TfactoryTeamflow\Http\Livewire\SendMessage::class);
+            // Livewire::component('log-note', \Techsfactory\TfactoryTeamflow\Http\Livewire\LogNote::class);
+            // Livewire::component('attachment-list', \Techsfactory\TfactoryTeamflow\Http\Livewire\AttachmentList::class);
+            // Livewire::component('chatter-tabs', \Techsfactory\TfactoryTeamflow\Http\Livewire\ChatterTabs::class);
         }
-    }
-
-    protected function runMigrations()
-    {
-        // Run migrations using the Artisan command
-        \Artisan::call('migrate', ['--force' => true]);
     }
 }
