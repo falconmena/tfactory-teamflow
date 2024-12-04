d31e5760fec8a882d4054553231561d9de0879d6
yasir-alobaidi <yasser.alobaydy@gmail.com>
2024-12-04 15:04:52 +0300

    split the view and include it

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,6 +1,47 @@
-<div class="send-message-box">
-    <form action="#">
-        <textarea name="message" placeholder="Enter your message here..."></textarea>
-        <button type="submit">Send Message</button>
-    </form>
+<div class="tab-pane active" id="send-message" role="tabpanel">
+    <div class="row px-2">
+        <div class="col-12 d-flex">
+            <div class="avatar avatar-2xl">
+                <img class="rounded-circle" src="../../assets/img/team/1.jpg" alt="" />
+            </div>
+            <div class="flex-1 ms-2">
+                <h6 class="mb-0 fs-0">Tasneem Aldirini</h6>
+                <a class="text-800 fs--1" href="#!">
+                    <span class="fw-semi-bold">Sales</span>
+                    <span class="ms-1 text-500">&lt;t.dirini@techs.com&gt;</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-12 mt-3 mb-2">
+            <form action="">
+                <p class="text-warning fs--2 mb-2">To: Followers of "[T7805] Test Case Round one"</p>
+                <textarea class="tinymce d-none fs--1 border" data-tinymce="data-tinymce" name="content"></textarea>
+                <div class="text-end mt-3">
+                    <button class="btn btn-primary px-4 py-2 fs--1"><span class="d-inline-block mx-2">Send </span> <span class="fab fa-telegram-plane"></span></button>
+                </div>
+            </form>
+        </div>
+        {{-- <div class="col-md-auto ms-auto d-flex align-items-center ps-6 ps-md-3"><small>8:40 AM (9 hours ago)</small><span class="fas fa-star text-warning fs--1 ms-2"></span></div> --}}
+    </div>
+</div>
+<div class="tab-pane" id="log-note" role="tabpanel">
+    <div class="row px-2">
+        <div class="col-12 d-flex">
+            <div class="avatar avatar-2xl">
+                <img class="rounded-circle" src="../../assets/img/team/1.jpg" alt="" />
+            </div>
+            <div class="flex-1 ms-2">
+                <h6 class="mb-0 fs-0">Tasneem Aldirini</h6><a class="text-800 fs--1" href="#!"><span class="fw-semi-bold">Sales</span><span class="ms-1 text-500">&lt;t.dirini@techs.com&gt;</span></a>
+            </div>
+        </div>
+        <div class="col-12 mt-3 mb-2">
+            <form action="">
+                <textarea class="tinymce d-none fs--1 border" data-tinymce="data-tinymce" name="content"></textarea>
+                <div class="text-end mt-3">
+                    <button class="btn btn-primary px-4 py-2 fs--1"> <span class="d-inline-block mx-2">Send </span> <span class="fab fa-telegram-plane"></span></button>
+                </div>
+            </form>
+        </div>
+        {{-- <div class="col-md-auto ms-auto d-flex align-items-center ps-6 ps-md-3"><small>8:40 AM (9 hours ago)</small><span class="fas fa-star text-warning fs--1 ms-2"></span></div> --}}
+    </div>
 </div>
