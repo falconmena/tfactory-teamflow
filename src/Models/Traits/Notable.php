a867f28436402a58c4d6dc2cb8dc5bf4bf1e25bd
a.almoghrabi <a.almoghrabi@techsfactory.com>
2024-12-04 17:24:31 +0300

    add notable trait

---
+++ src/Models/Traits/Notable.php
@@ -0,0 +1,21 @@
+<?php
+namespace Techsfactory\TfactoryTeamflow\Models;
+
+Trait Notable
+{
+    public function messages()
+    {
+        return Note::query()
+                ->where('notable_type','=',self::class)
+                ->where('type','=','message')
+                ->where('notable_id','=',$this->id);
+    }
+
+    public function notes()
+    {
+        return Note::query()
+                ->where('notable_type','=',self::class)
+                ->where('type','=','note')
+                ->where('notable_id','=',$this->id);
+    }
+}
