42accfc56cdaf21474045dfbeeedded6ae9b8bb1
ammarzyoud <ammar.m.alzyoud@gmail.com>
2024-12-05 11:58:08 +0300

    Attachment

--- src/Models/Attachment.php
+++ src/Models/Attachment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Attachment extends Model
 {
     protected $table = 'tf_teamflow_attachments';
-    protected $fillable = ['media_type', 'media_path'];
+    protected $fillable = ['media_type', 'media_path', 'attachable_type', 'attachable_id'];
 
     public function attachable()
     {
