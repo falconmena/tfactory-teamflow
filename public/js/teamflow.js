d9a4275efa0ae948b068bc19c082d72d4e4ad02d
ammarzyoud <ammar.m.alzyoud@gmail.com>
2024-12-05 13:42:00 +0300

    Attachment

--- public/js/teamflow.js
+++ public/js/teamflow.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     // Init dropzone
     dropzoneInit();
-    getRecentAttachments();
 
     $('#teamflowForm').on('submit', function (e) {
         e.preventDefault();
@@ -25,75 +24,150 @@ $(document).ready(function () {
     });
 });
 
-// Attachment Dropzone
+window.Dropzone ? (window.Dropzone.autoDiscover = false) : "";
+
 const dropzoneInit = () => {
-    const dropzoneElement = document.querySelector("#teamflow-data-dropzone-attachment");
-    if (dropzoneElement.dropzone) return; // Skip if already initialized
+    const { merge } = window._;
 
-    const myDropzone = new Dropzone(dropzoneElement, {
-        url: dropzoneElement.action,
-        method: "POST",
-        paramName: "file",
-        autoProcessQueue: false, // Prevent automatic upload
-        parallelUploads: 10, // Number of files processed in parallel
-        maxFilesize: 20, // Max size in MB
-        headers: {
-            "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value,
-        },
-        init: function () {
-            const submitButton = document.querySelector("#submit-button");
+    const Selector = {
+        DROPZONE: "#teamflow-data-dropzone-attachment",
+        DZ_ERROR_MESSAGE: ".dz-error-message",
+        DZ_PREVIEW: ".dz-preview",
+        DZ_PROGRESS: ".dz-preview .dz-preview-cover .dz-progress",
+        DZ_PREVIEW_COVER: ".dz-preview .dz-preview-cover",
+    };
 
-            submitButton.addEventListener("click", (e) => {
-                e.preventDefault();
+    const ClassName = {
+        DZ_FILE_PROCESSING: "dz-file-processing",
+        DZ_FILE_COMPLETE: "dz-file-complete",
+        DZ_COMPLETE: "dz-complete",
+        DZ_PROCESSING: "dz-processing",
+    };
 
-                if (this.getQueuedFiles().length > 0) {
-                    this.processQueue(); // Manually trigger file upload
-                } else {
-                    alert("No files to upload.");
-                }
-            });
+    const DATA_KEY = {
+        OPTIONS: "options",
+    };
 
-            this.on("sending", function (file, xhr, formData) {
-                // Append additional data to the request
-                formData.append("attachable_id", document.querySelector('input[name="attachable_id"]').value);
-                formData.append("attachable_type", document.querySelector('input[name="attachable_type"]').value);
-                formData.append("created_by", document.querySelector('input[name="created_by"]').value);
-            });
+    const Events = {
+        ADDED_FILE: "addedfile",
+        REMOVED_FILE: "removedfile",
+        COMPLETE: "complete",
+    };
 
-            this.on("queuecomplete", function () {
-                alert("All files have been uploaded successfully!");
-            });
+    const dropzones = document.querySelector(Selector.DROPZONE);
 
-            this.on("error", function (file, errorMessage) {
-                alert(`Error uploading file: ${errorMessage}`);
-            });
-        },
-    });
+    !!dropzones.length &&
+        dropzones.forEach((item) => {
+            let userOptions = utils.getData(item, DATA_KEY.OPTIONS);
+            userOptions = userOptions ? userOptions : {};
+            const data = userOptions.data ? userOptions.data : {};
+            const options = merge(
+                {
+                    url: dropzoneElement.action,
+                    method: "POST",
+                    paramName: "file",
+                    headers: {
+                        "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value,
+                    },
+                    addRemoveLinks: false,
+                    previewsContainer: item.querySelector(Selector.DZ_PREVIEW),
+                    previewTemplate: item.querySelector(Selector.DZ_PREVIEW)
+                        .innerHTML,
+                    thumbnailWidth: null,
+                    thumbnailHeight: null,
+                    maxFilesize: 20,
+                    autoProcessQueue: false,
+                    filesizeBase: 1000,
+                    init: function init() {
+                        const thisDropzone = this;
 
-    // Handle file removal
-    myDropzone.on("removedfile", function (file) {
-        // Optional: Perform an AJAX request to delete the file
-        const fileId = file.previewElement.querySelector(".dz-remove").dataset.fileId;
-        if (fileId) {
-            fetch(`/teamflow/attachments/delete/${fileId}`, {
-                method: "DELETE",
-                headers: {
-                    "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value,
+                        if (data.length) {
+                            data.forEach((v) => {
+                                const mockFile = { name: v.name, size: v.size };
+                                thisDropzone.options.addedfile.call(
+                                    thisDropzone,
+                                    mockFile
+                                );
+                                thisDropzone.options.thumbnail.call(
+                                    thisDropzone,
+                                    mockFile,
+                                    `${v.url}/${v.name}`
+                                );
+                            });
+                        }
+
+                        thisDropzone.on(
+                            Events.ADDED_FILE,
+                            function addedfile() {
+                                if ("maxFiles" in userOptions) {
+                                    if (
+                                        userOptions.maxFiles === 1 &&
+                                        item.querySelectorAll(
+                                            Selector.DZ_PREVIEW_COVER
+                                        ).length > 1
+                                    ) {
+                                        item.querySelector(
+                                            Selector.DZ_PREVIEW_COVER
+                                        ).remove();
+                                    }
+                                    if (
+                                        userOptions.maxFiles === 1 &&
+                                        this.files.length > 1
+                                    ) {
+                                        this.removeFile(this.files[0]);
+                                    }
+                                }
+                            }
+                        );
+                    },
+                    error(file, message) {
+                        if (file.previewElement) {
+                            file.previewElement.classList.add("dz-error");
+                            if (typeof message !== "string" && message.error) {
+                                message = message.error;
+                            }
+                            for (let node of file.previewElement.querySelectorAll(
+                                "[data-dz-errormessage]"
+                            )) {
+                                node.textContent = message;
+                            }
+                        }
+                    },
                 },
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(`File deleted: ${file.name}`);
-                })
-                .catch((error) => {
-                    console.error("Error deleting file:", error);
-                });
-        }
-    });
-};
+                userOptions
+            );
+            // eslint-disable-next-line
+            item.querySelector(Selector.DZ_PREVIEW).innerHTML = "";
 
-const getRecentAttachments = () => {
-    // Fetch recently uploaded files
+            const dropzone = new window.Dropzone(item, options);
+
+            dropzone.on(Events.ADDED_FILE, () => {
+                if (item.querySelector(Selector.DZ_PREVIEW_COVER)) {
+                    item.querySelector(
+                        Selector.DZ_PREVIEW_COVER
+                    ).classList.remove(ClassName.DZ_FILE_COMPLETE);
+                }
+                item.classList.add(ClassName.DZ_FILE_PROCESSING);
+            });
+            dropzone.on(Events.REMOVED_FILE, () => {
+                if (item.querySelector(Selector.DZ_PREVIEW_COVER)) {
+                    item.querySelector(
+                        Selector.DZ_PREVIEW_COVER
+                    ).classList.remove(ClassName.DZ_PROCESSING);
+                }
+                item.classList.add(ClassName.DZ_FILE_COMPLETE);
+            });
+            dropzone.on(Events.COMPLETE, () => {
+                if (item.querySelector(Selector.DZ_PREVIEW_COVER)) {
+                    item.querySelector(
+                        Selector.DZ_PREVIEW_COVER
+                    ).classList.remove(ClassName.DZ_PROCESSING);
+                }
+
+                item.classList.add(ClassName.DZ_FILE_COMPLETE);
+            });
+        });
+
     fetch("/teamflow/attachments/recent")
         .then((response) => response.json())
         .then((files) => {
@@ -113,3 +187,70 @@ const getRecentAttachments = () => {
             console.error("Error fetching recent files:", error);
         });
 };
+
+// // Attachment Dropzone
+// const dropzoneInit = () => {
+//     const dropzoneElement = document.querySelector("#teamflow-data-dropzone-attachment");
+//     if (dropzoneElement.dropzone) return; // Skip if already initialized
+
+//     const myDropzone = new Dropzone(dropzoneElement, {
+//         url: dropzoneElement.action,
+//         method: "POST",
+//         paramName: "file",
+//         autoProcessQueue: false, // Prevent automatic upload
+//         parallelUploads: 10, // Number of files processed in parallel
+//         maxFilesize: 20, // Max size in MB
+//         headers: {
+//             "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value,
+//         },
+//         init: function () {
+//             const submitButton = document.querySelector("#submit-button");
+
+//             submitButton.addEventListener("click", (e) => {
+//                 e.preventDefault();
+
+//                 if (this.getQueuedFiles().length > 0) {
+//                     this.processQueue(); // Manually trigger file upload
+//                 } else {
+//                     alert("No files to upload.");
+//                 }
+//             });
+
+//             this.on("sending", function (file, xhr, formData) {
+//                 // Append additional data to the request
+//                 formData.append("attachable_id", document.querySelector('input[name="attachable_id"]').value);
+//                 formData.append("attachable_type", document.querySelector('input[name="attachable_type"]').value);
+//                 formData.append("created_by", document.querySelector('input[name="created_by"]').value);
+//             });
+
+//             this.on("queuecomplete", function () {
+//                 alert("All files have been uploaded successfully!");
+//             });
+
+//             this.on("error", function (file, errorMessage) {
+//                 alert(`Error uploading file: ${errorMessage}`);
+//             });
+//         },
+//     });
+
+//     // Handle file removal
+//     myDropzone.on("removedfile", function (file) {
+//         // Optional: Perform an AJAX request to delete the file
+//         const fileId = file.previewElement.querySelector(".dz-remove").dataset.fileId;
+//         if (fileId) {
+//             fetch(`/teamflow/attachments/delete/${fileId}`, {
+//                 method: "DELETE",
+//                 headers: {
+//                     "X-CSRF-TOKEN": document.querySelector('input[name="_token"]').value,
+//                 },
+//             })
+//                 .then((response) => response.json())
+//                 .then((data) => {
+//                     console.log(`File deleted: ${file.name}`);
+//                 })
+//                 .catch((error) => {
+//                     console.error("Error deleting file:", error);
+//                 });
+//         }
+//     });
+// };
