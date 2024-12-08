81eac14776cd2130d6f933d6d58b13797cb38b4a
ammarzyoud <ammar.m.alzyoud@gmail.com>
2024-12-08 11:44:14 +0300

    Activity

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     protected $table = 'tf_teamflow_activites';
-    protected $fillable = ['activityable_type', 'activityable_id', 'assigned_to', 'title', 'due_date', 'activity_type', 'description'];
+    protected $fillable = ['activityable_type', 'activityable_id', 'assigned_to', 'title', 'due_date', 'activity_type', 'description', 'created_by'];
 
     public function activityable()
     {
