b0b45510d264828a3c215645a8872b5611deeea5
ammarzyoud <ammar.m.alzyoud@gmail.com>
2024-12-08 11:37:42 +0300

    Activity

--- src/Http/Controllers/ActivityController.php
+++ src/Http/Controllers/ActivityController.php
@@ -35,6 +35,7 @@ class ActivityController extends Controller
 
     public function store(Request $request)
     {
+        dd($request);
         $request->validate([
             'activityable_id' => 'required',
             'activityable_type' => 'required',
